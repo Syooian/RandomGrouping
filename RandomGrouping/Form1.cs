22253b0c8d09e76e7f9f83f13823e114cb68c253
Syooian <msn0126@hotmail.com>
2025-07-04 19:12:33 +0800

    讀取設定檔並反映到座位勾選狀態

--- RandomGrouping/Form1.cs
+++ RandomGrouping/Form1.cs
@@ -1,4 +1,7 @@
+using System.ComponentModel.Design.Serialization;
+using System.Diagnostics;
 using System.Reflection.Metadata;
+using System.Text.Json;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace RandomGrouping
@@ -9,7 +12,11 @@ namespace RandomGrouping
         {
             InitializeComponent();
 
+            LoadSettings();
+
             InitPanel();
+
+
         }
 
         /// <summary>
@@ -28,15 +35,83 @@ namespace RandomGrouping
                     Text = (a + 1).ToString(),
                     Size = new Size(170, 35),
                     BackColor = Color.FromArgb(255, 255, 190),
+                    Checked= CurrentSettings.IsSeatClick[a]
+                };
+
+                var SeatID = a;
+                SeatList[a].CheckedChanged += (sender, e) =>
+                {
+                    CurrentSettings.IsSeatClick[SeatID] = SeatList[SeatID].Checked;
+
+                    SaveSettings();
                 };
 
                 SeatsLayoutPanel.Controls.Add(SeatList[a]);
             }
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        Settings CurrentSettings;
+        /// <summary>
+        /// 
+        /// </summary>
+        const string SettingsFileName = "Settings.text";
+        /// <summary>
+        /// 
+        /// </summary>
+        void LoadSettings()
+        {
+            if (File.Exists(SettingsFileName))
+            {
+                try
+                {
+                    CurrentSettings = JsonSerializer.Deserialize<Settings>(File.ReadAllText(SettingsFileName));
+
+                    SeatList = new CheckBox[CurrentSettings.IsSeatClick.Length];
+                    for (int a = 0; a < CurrentSettings.IsSeatClick.Length; a++)
+                    {
+                        SeatList[a].Checked = CurrentSettings.IsSeatClick[a];
+                    }
+
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"讀取設定檔失敗: {ex.Message}");
+                }
+            }
+
+            //沒有設定檔，或是設定檔讀取錯誤時，使用預設設定
+            CurrentSettings = new Settings
+            {
+                IsSeatClick = Enumerable.Repeat(true, 30).ToArray()
+            };
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        void SaveSettings()
+        {
+            var Json = JsonSerializer.Serialize(CurrentSettings);
+            Debug.WriteLine(Json);
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        /// <summary>
+        /// 設定檔結構
+        /// </summary>
+        struct Settings
+        {
+            /// <summary>
+            /// 座號是否勾選
+            /// </summary>
+            public bool[] IsSeatClick { get; set; }
         }
 
         //    function shuffleAndGroup()
